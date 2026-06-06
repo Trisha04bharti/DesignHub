7dce2624478e93aa26bd40410bc324d30857fd7a
Trisha Bharti <trishabharti444@gmail.com>
2026-06-06 12:39:17 +0530

    created an custom tragnle shape

--- DesignHub/ShapeDesign/triangle.swift
+++ DesignHub/ShapeDesign/triangle.swift
@@ -7,13 +7,26 @@
 
 import SwiftUI
 
-struct Triangle: Shape {
+struct Diamond: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
 
         path.move(to: CGPoint(x: rect.midX, y: rect.minY))     // Top
         path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))  // Bottom right
         path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))  // Bottom left
+        path.closeSubpath()
+
+        return path
+    }
+}
+
+struct DownTriangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.midX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
         path.closeSubpath()
 
         return path
